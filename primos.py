756923394191e0402c5a7ac67b9e21611b3e9d8a
Matheus <matheussteinbach11@gmail.com>
2025-07-16 20:24:38 -0300

    feat: adiciona primos por recursividade e refactor em funcao primo

--- primos.py
+++ primos.py
@@ -1,24 +1,33 @@
 
 
-def primos_recursivo(n):
-    pass
+def primo(n):
+    divisores = 0
+    for j in range(1, n + 1):
+        if n % j == 0:
+            divisores += 1
 
+    # Divisivel por 1 e por ele mesmo
+    return divisores == 2
+
+
+def primos_recursivo(n):
+    if n == 2:
+        return 
+    else:
+        if primo(n):
+            primos_rec.append(n)
+    primos_recursivo(n - 1)
+    return sorted(primos_rec)
 
 def primos_linear(n):
-    primos = [2]
+    primos_lin = [2]
     if n == 2:
-        return primos
+        return primos_lin
     else:
         for i in range(3, n + 1):
-            divisores = 0
-            for j in range(1, i + 1):
-                if i % j == 0:
-                    divisores += 1
-
-            # Divisivel por 1 e por ele mesmo
-            if divisores == 2:
-                primos.append(i)
-        return primos
+            if primo(i):
+                primos_lin.append(i)
+        return primos_lin
 
 while True:
     n = input("Insira o valor de N: ")
@@ -30,5 +39,6 @@ while True:
         pass
     print("O valor de N deve ser um número inteiro maior que um!")
 
+primos_rec = [2]
 print('Primos recursivo = ', primos_recursivo(n))
 print('Primos linear = ', primos_linear(n))
